285408934dafa7c42956bd822acd0cfc5082e60a
Mengdi Wang <wangmengdi0701@hotmail.com>
2021-05-27 02:15:47 +0200

    minimap added

---
+++ CloudVis/Assets/Scripts/MinimapCameraMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCameraMovement : MonoBehaviour
+{
+
+    public Camera m_MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    // LateUpdate is called once per frame after Update and FixedUpdate
+    void LateUpdate()
+    {
+        Vector3 currentPosition = m_MainCamera.transform.position;
+
+        // keep y component
+        currentPosition.y = transform.position.y;
+
+        transform.position = currentPosition;
+
+        transform.rotation = Quaternion.Euler(90f, m_MainCamera.transform.eulerAngles.y, 0f);
+    }
+}
