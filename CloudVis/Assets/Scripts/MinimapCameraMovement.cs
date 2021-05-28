4696d63c8ed6e4d8067003e84b5c424bd94a6a58
Mengdi Wang <wangmengdi0701@hotmail.com>
2021-05-28 19:53:22 +0200

    update minimap text

--- CloudVis/Assets/Scripts/MinimapCameraMovement.cs
+++ CloudVis/Assets/Scripts/MinimapCameraMovement.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 public class MinimapCameraMovement : MonoBehaviour
 {
 
+    public float m_MinimapCameraHeight = 200.0f;
     private Camera m_MainCamera;
+    private Vector3 currentPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         m_MainCamera = Camera.main;
+        
+        currentPosition = m_MainCamera.transform.position;
+
+        // keep y component
+        currentPosition.y = m_MinimapCameraHeight;
+
+        transform.position = currentPosition;
+
+        transform.rotation = Quaternion.Euler(90f, m_MainCamera.transform.eulerAngles.y, 0f);
     }
 
     // LateUpdate is called once per frame after Update and FixedUpdate
     void LateUpdate()
     {
-        Vector3 currentPosition = m_MainCamera.transform.position;
+        currentPosition = m_MainCamera.transform.position;
 
         // keep y component
         currentPosition.y = transform.position.y;
--- CloudVis/Assets/Scripts/MinimapText.cs
+++ CloudVis/Assets/Scripts/MinimapText.cs
@@ -5,20 +5,36 @@ using UnityEngine.UI;
 
 public class MinimapText : MonoBehaviour
 {
+    public Terrain m_Terrmain;
+    public float lon_min = 4.5f;
+    public float lon_max = 14.496f;
+    public float lat_min = 47.5f;
+    public float lat_max = 54.4975f;
     private Camera m_MainCamera;
     private Text minimapText;
- 
+    private float x_range;
+    private float z_range;
+    private float lon_range;
+    private float lat_range;
+
     // Start is called before the first frame update
     void Start()
     {
         m_MainCamera = Camera.main;
         minimapText = GetComponent<Text>();
+        x_range = m_Terrmain.GetComponent<Terrain>().terrainData.size.x;
+        z_range = m_Terrmain.GetComponent<Terrain>().terrainData.size.z;
+        lon_range = lon_max - lon_min;
+        lat_range = lat_max - lat_min;
     }
 
     // Update is called once per frame
     void Update()
     {
-        minimapText.text = "lon: " + m_MainCamera.transform.position.x.ToString("F2") + "   " + 
-                           "lat: " + m_MainCamera.transform.position.z.ToString("F2");
+        float lon = (m_MainCamera.transform.position.x / x_range + 0.5f) * lon_range + lon_min;
+        float lat = (m_MainCamera.transform.position.z / z_range + 0.5f) * lat_range + lat_min;
+        minimapText.text = "lon: " + lon.ToString("F2") + "\n" + 
+                           "lat: " + lat.ToString("F2") + "\n" + 
+                           "ht: " + m_MainCamera.transform.position.y.ToString("F2");
     }
 }
